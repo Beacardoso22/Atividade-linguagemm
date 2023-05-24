d27d56670c64f982d488c2e4e1c9a5acec481904
DanzinNN <danielguimaraessouza8@gmail.com>
2023-05-24 17:49:33 -0400

    att

--- lib/components/my_bottom_nav_bar.dart
+++ lib/components/my_bottom_nav_bar.dart
@@ -1,29 +1,49 @@
+//importando algumas coisas
 import 'package:flutter/material.dart';
+//contém widgets ligados ao design, ajudará na construção da interface
 import 'package:flutter_svg/flutter_svg.dart';
-
+//para exibir imagens em SVG no flutter
 import '../constants.dart';
+//é um bliblioteca de estilo, cores, estilos e tamanhos para deixar mais separado e facilitar a reutilizar o códido
 
 class MyBottomNavBar extends StatelessWidget {
+  /*Classe com seu nome MyBottomNavBar que
+  se extende com StatelessWidget vai passar ser um widget
+  e não vai poder mexer na tela do usuário*/
   const MyBottomNavBar({
+    /*contrutor da classe com o nome MyBottomNavBar*/
     Key key,
+    //parâmentro key opcional
   }) : super(key: key);
+  //chama o construtor da classe e passa o parâmetro para ele
 
   @override
+  //usado para mostrar que está substituindo um metodo na clase pai
   Widget build(BuildContext context) {
+    /*contrutor da interfase, o método obrigatório de um widget
+    o parâmentro dentro é usado para acessar as informações*/
     return Container(
+      //retornando o container que vai representar a barra de navegação na parte inferior
       padding: EdgeInsets.only(
+        /*define o espaçamento do container, 
+        EdgeInsets faz o tipo de espaçamento se é para a direita ou esqueda*/
         left: kDefaultPadding * 2,
         right: kDefaultPadding * 2,
         bottom: kDefaultPadding,
+        //as direções sendo multíplicados por 2 através da constante kDefaultPadding
       ),
       height: 80,
+      //altura
       decoration: BoxDecoration(
+        //decoração
         color: Colors.white,
+        //cor de fundo
         boxShadow: [
           BoxShadow(
             offset: Offset(0, -10),
             blurRadius: 35,
             color: kPrimaryColor.withOpacity(0.38),
+            //uma propriedade de desfoque
           ),
         ],
       ),
@@ -41,6 +61,9 @@ class MyBottomNavBar extends StatelessWidget {
           IconButton(
             icon: SvgPicture.asset("assets/icons/user-icon.svg"),
             onPressed: () {},
+            /*e uma subclasse do container
+            que contem o row que contém 3 icones para navegação na barra 
+            */
           ),
         ],
       ),
--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,16 +1,29 @@
+//importando bibliotecas
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
 
 import 'image_and_icons.dart';
 import 'title_and_price.dart';
+//widgets e classes ligados ao design do flutter
 
 class Body extends StatelessWidget {
+//classe Body estende a StatelessWidget algo que não
+//pode ser mudado
   @override
+  //usado quando está modificando algum metodo
   Widget build(BuildContext context) {
+    //construtor da interface build obrigatório para dar certo
+
     Size size = MediaQuery.of(context).size;
+    //tamanho da tela
     return SingleChildScrollView(
+      //retorna um widget
+      //permite a rolagem de conteúdo caso
+      //exceda o tamanho da tela
       child: Column(
+        //cria uma cola que vai ser empilhada na vertical
         children: <Widget>[
+          //mostra um widget
           ImageAndIcons(size: size),
           TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
           SizedBox(height: kDefaultPadding),
